e3ef4f88d6dea6b97e5651bebeb16c2a32c6c7bd
dinhkhiemtran <dinhkhiemtran@gmail.com>
2023-09-09 17:28:47 +0700

    NeedForSpeed

---
+++ src/main/java/org/khiemtran/constructors/NeedForSpeed.java
@@ -0,0 +1,52 @@
+package org.khiemtran.constructors;
+
+public class NeedForSpeed {
+  private static final int SPEED_OF_METERS = 50;
+  private static final int BATTERY_DRAIN = 4;
+  private int meter;
+  private int battery;
+  private int speed;
+  private int batteryDrain;
+
+  public NeedForSpeed(int speed, int batteryDrain) {
+    this.speed = speed;
+    this.batteryDrain = batteryDrain;
+    this.meter = 0;
+    this.battery = 100;
+  }
+
+  public boolean batteryDrained() {
+    return battery <= 0;
+  }
+
+  public int distanceDriven() {
+    return meter;
+  }
+
+  public void drive() {
+    if (!batteryDrained()) {
+      meter += speed;
+      battery -= batteryDrain;
+    }
+  }
+
+  public static NeedForSpeed nitro() {
+    NeedForSpeed car = new NeedForSpeed(SPEED_OF_METERS, BATTERY_DRAIN);
+    return car;
+  }
+}
+
+class RaceTrack {
+  private int distance;
+
+  RaceTrack(int distance) {
+    this.distance = distance;
+  }
+
+  public boolean tryFinishTrack(NeedForSpeed car) {
+    while (car.distanceDriven() < distance && !car.batteryDrained()) {
+      car.drive();
+    }
+    return car.distanceDriven() >= distance;
+  }
+}
---
+++ src/test/java/org/khiemtran/constructors/NeedForSpeedTest.java
@@ -0,0 +1,139 @@
+package org.khiemtran.constructors;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class NeedForSpeedTest {
+  @Test
+  public void new_remote_control_car_has_not_driven_any_distance() {
+    int speed = 10;
+    int batteryDrain = 2;
+    var car = new NeedForSpeed(speed, batteryDrain);
+    Assertions.assertEquals(0, car.distanceDriven());
+  }
+
+  @Test
+  public void drive_until_battery_is_drained() {
+    int speed = 2;
+    int batteryDrain = 1;
+    var car = new NeedForSpeed(speed, batteryDrain);
+    // Drain the battery
+    for (var i = 0; i < 100; i++) {
+      car.drive();
+    }
+    Assertions.assertEquals(true, car.batteryDrained());
+  }
+
+  @Test
+  public void nitro_car_has_battery_not_drained() {
+    var car = NeedForSpeed.nitro();
+    Assertions.assertEquals(false, car.batteryDrained());
+  }
+
+  @Test
+  public void new_remote_control_car_battery_is_not_drained() {
+    int speed = 15;
+    int batteryDrain = 3;
+    var car = new NeedForSpeed(speed, batteryDrain);
+    Assertions.assertEquals(false, car.batteryDrained());
+  }
+
+  @Test
+  public void car_can_finish_with_car_that_just_cannot_finish() {
+    int speed = 3;
+    int batteryDrain = 20;
+    var car = new NeedForSpeed(speed, batteryDrain);
+    int distance = 16;
+    var race = new RaceTrack(distance);
+    Assertions.assertEquals(false, race.tryFinishTrack(car));
+  }
+
+  @Test
+  public void drive_increases_distance_driven_with_speed() {
+    int speed = 5;
+    int batteryDrain = 1;
+    var car = new NeedForSpeed(speed, batteryDrain);
+    car.drive();
+    Assertions.assertEquals(5, car.distanceDriven());
+  }
+
+  @Test
+  public void car_can_finish_with_car_that_can_easily_finish() {
+    int speed = 10;
+    int batteryDrain = 2;
+    var car = new NeedForSpeed(speed, batteryDrain);
+    int distance = 100;
+    var race = new RaceTrack(distance);
+    Assertions.assertEquals(true, race.tryFinishTrack(car));
+  }
+
+  @Test
+  public void drive_does_not_increase_distance_driven_when_battery_drained() {
+    int speed = 9;
+    int batteryDrain = 50;
+    var car = new NeedForSpeed(speed, batteryDrain);
+    // Drain the battery
+    car.drive();
+    car.drive();
+    car.drive();
+    Assertions.assertEquals(18, car.distanceDriven());
+  }
+
+  @Test
+  public void nitro_has_correct_battery_drain() {
+    var car = NeedForSpeed.nitro();
+    // The battery is almost drained
+    for (var i = 0; i < 24; i++) {
+      car.drive();
+    }
+    Assertions.assertEquals(false, car.batteryDrained());
+    // Drain the battery
+    car.drive();
+    Assertions.assertEquals(true, car.batteryDrained());
+  }
+
+  @Test
+  public void car_can_finish_with_car_that_can_just_finish() {
+    int speed = 2;
+    int batteryDrain = 10;
+    var car = new NeedForSpeed(speed, batteryDrain);
+    int distance = 20;
+    var race = new RaceTrack(distance);
+    Assertions.assertEquals(true, race.tryFinishTrack(car));
+  }
+
+  @Test
+  public void nitro_car_has_correct_speed() {
+    var car = NeedForSpeed.nitro();
+    car.drive();
+    Assertions.assertEquals(50, car.distanceDriven());
+  }
+
+  @Test
+  public void nitro_car_has_not_driven_any_distance() {
+    var car = NeedForSpeed.nitro();
+    Assertions.assertEquals(0, car.distanceDriven());
+  }
+
+  @Test
+  public void car_can_finish_with_car_that_cannot_finish() {
+    int speed = 1;
+    int batteryDrain = 20;
+    var car = new NeedForSpeed(speed, batteryDrain);
+    int distance = 678;
+    var race = new RaceTrack(distance);
+    Assertions.assertEquals(false, race.tryFinishTrack(car));
+  }
+
+  @Test
+  public void drive_to_almost_drain_battery() {
+    int speed = 2;
+    int batteryDrain = 1;
+    var car = new NeedForSpeed(speed, batteryDrain);
+    // Almost drain the battery
+    for (var i = 0; i < 99; i++) {
+      car.drive();
+    }
+    Assertions.assertEquals(false, car.batteryDrained());
+  }
+}
