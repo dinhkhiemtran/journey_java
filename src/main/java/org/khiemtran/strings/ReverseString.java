9054fdcd85ab386b7fcd4250d962c37f7f3d7b3d
Khiem Tran <tdkhiem>
2023-11-07 15:04:13 +0700

    ReverseString

---
+++ src/main/java/org/khiemtran/strings/ReverseString.java
@@ -0,0 +1,7 @@
+package org.khiemtran.strings;
+
+public class ReverseString {
+  public String reverse(String inputString) {
+    return new StringBuilder(inputString).reverse().toString();
+  }
+}
